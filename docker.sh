8ed719d22693407899db9aa588b5a08f032170e0
Eirini <49712302+inferential@users.noreply.github.com>
2021-04-04 19:26:03 +0100

    Docker
    I removed Docker-related scripts, as they complicated the setup. I'm keeping it simple for now

--- docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Running app with Docker"
-./julia-1.6.0/bin/julia --project src/docker.jl
--- src/docker.jl
+++
@@ -1,7 +0,0 @@
-using Genie.Deploy
-
-print("Please wait, buidling Docker container... \n")
-Deploy.Docker.build()
-
-print("Running Docker container \n")
-Deploy.Docker.run()
